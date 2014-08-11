b7f4afa7690663239be4b275338e701c66d92582
unknown <F000JW7@MECHA-3.kiewit.dartmouth.edu>
2014-08-11 14:30:09 -0400

    testing push

---
+++ OctaveKeyboard/DDS.vhd
@@ -0,0 +1,42 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date:    14:29:30 08/11/2014 
+-- Design Name: 
+-- Module Name:    DDS - Behavioral 
+-- Project Name: 
+-- Target Devices: 
+-- Tool versions: 
+-- Description: 
+--
+-- Dependencies: 
+--
+-- Revision: 
+-- Revision 0.01 - File Created
+-- Additional Comments: 
+--
+----------------------------------------------------------------------------------
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx primitives in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity DDS is
+    Port ( clk : in  STD_LOGIC);
+end DDS;
+
+architecture Behavioral of DDS is
+
+begin
+
+
+end Behavioral;
+
