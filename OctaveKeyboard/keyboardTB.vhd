c3670451c83efe5c88358088afeb857233a10651
unknown <F000JW7@MECHA-5.kiewit.dartmouth.edu>
2014-08-25 14:23:59 -0400

    fixed sim

--- OctaveKeyboard/keyboardTB.vhd
+++ OctaveKeyboard/keyboardTB.vhd
@@ -2,9 +2,9 @@
 -- Company: 
 -- Engineer:
 --
--- Create Date:   15:16:28 08/17/2014
+-- Create Date:   14:17:41 08/25/2014
 -- Design Name:   
--- Module Name:   C:/Users/F000JW7/Desktop/octave-keyboard/OctaveKeyboard/KeyboardTB.vhd
+-- Module Name:   C:/Users/F000JW7/Desktop/octave-keyboard/OctaveKeyboard/keyboardTB.vhd
 -- Project Name:  OctaveKeyboard
 -- Target Device:  
 -- Tool versions:  
@@ -28,53 +28,54 @@
 LIBRARY ieee;
 USE ieee.std_logic_1164.ALL;
  
-ENTITY KeyboardTB IS
-END KeyboardTB;
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--USE ieee.numeric_std.ALL;
  
-ARCHITECTURE behavior OF KeyboardTB IS 
+ENTITY keyboardTB IS
+END keyboardTB;
+ 
+ARCHITECTURE behavior OF keyboardTB IS 
  
     -- Component Declaration for the Unit Under Test (UUT)
  
     COMPONENT OctaveKeyboardTop
     PORT(
-         keys : IN  std_logic_vector(7 downto 0);
          clk : IN  std_logic;
+         keys : IN  std_logic_vector(7 downto 0);
          led_disable : IN  std_logic;
          song_enable : IN  std_logic;
          tone : OUT  std_logic;
          shutdown : OUT  std_logic;
-         key_out : OUT  std_logic_vector(7 downto 0);
          led_out : OUT  std_logic_vector(7 downto 0)
         );
     END COMPONENT;
     
 
    --Inputs
-   signal keys : std_logic_vector(7 downto 0) := (others => '0');
    signal clk : std_logic := '0';
+   signal keys : std_logic_vector(7 downto 0) := (others => '0');
    signal led_disable : std_logic := '0';
    signal song_enable : std_logic := '0';
 
  	--Outputs
    signal tone : std_logic;
    signal shutdown : std_logic;
-   signal key_out : std_logic_vector(7 downto 0);
    signal led_out : std_logic_vector(7 downto 0);
 
    -- Clock period definitions
-   constant clk_period : time := 1 ns;
+   constant clk_period : time := 10 ns;
  
 BEGIN
  
 	-- Instantiate the Unit Under Test (UUT)
    uut: OctaveKeyboardTop PORT MAP (
-          keys => keys,
           clk => clk,
+          keys => keys,
           led_disable => led_disable,
           song_enable => song_enable,
           tone => tone,
           shutdown => shutdown,
-          key_out => key_out,
           led_out => led_out
         );
 
@@ -92,12 +93,14 @@ BEGIN
    stim_proc: process
    begin		
       -- hold reset state for 100 ns.
+      wait for 100 ns;	
+
       wait for clk_period*10;
 
-		  keys <= "10000000";
-		  wait for clk_period*2;
-      		
-		wait;
+		keys <= "10000000";
+      -- insert stimulus here 
+
+      wait;
    end process;
 
 END;
